955b4a0ff0069bf27aeb021c0ec5f1ba9401dc12
amirreza <hshmtyamyrrda631@gmail.com>
2025-08-12 07:57:35 +0330

    wsgi

--- market/wsgi.py
+++ market/wsgi.py
@@ -8,9 +8,14 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+path = '/home/mahdiyemarket/market'
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'projectname.settings'
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'market.settings')
-
 application = get_wsgi_application()
+
